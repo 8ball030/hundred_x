0ae34e87211da0f2689847557f63f23025cfd396
kjr217 <55159119+kjr217@users.noreply.github.com>
2024-05-08 11:32:56 +0100

    fix tests with proper api

--- hundred_x/constants.py
+++ hundred_x/constants.py
@@ -7,8 +7,8 @@ from hundred_x.enums import ApiType, Environment
 DEVNET_REST_URL = "https://api.dev.ciaobella.dev"
 DEVNET_WEBSOCKET_URL = "https://stream.dev.ciaobella.dev"
 
-TESTNET_REST_URL = "https://staging.100x.finance"
-TESTNET_WEBSOCKET_URL = "https://staging.100x.finance"
+TESTNET_REST_URL = "https://api.staging.100x.finance"
+TESTNET_WEBSOCKET_URL = "https://stream.staging.100x.finance"
 
 MAINNET_REST_URL = "https://api.100x.finance"
 MAINNET_WEBSOCKET_URL = "https://stream.100x.finance"
@@ -35,14 +35,14 @@ LOGIN_MESSAGE = "I would like to login to 100x finance."
 CONTRACTS = {
     Environment.TESTNET: {
         "USDB": "0x79A59c326C715AC2d31C169C85d1232319E341ce",
-        "PROTOCOL": "0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937",
-        "VERIFYING_CONTRACT": "0x65CbB566D1A6E60107c0c7888761de1AdFa1ccC0",
+        "PROTOCOL": "0x0c3b9472b3923CfE199bAE24B5f5bD75FAD2bae9",
+        "VERIFYING_CONTRACT": "0x02Ca4fcB63E2D3C89fa20D86ccDcfc540c683545",
         "CHAIN_ID": 168587773,
     },
     Environment.DEVNET: {
         "USDB": "0x79A59c326C715AC2d31C169C85d1232319E341ce",
-        "PROTOCOL": "0x2c72Fbefcc6D23553EC4E209E7b23E118718a239",
-        "VERIFYING_CONTRACT": "0xFA322Cd45159384132C3D191618556349bA3bF53",
+        "PROTOCOL": "0x9E4d21FeFE19EbD9ce94b71A60bce6CD793DD2BF",
+        "VERIFYING_CONTRACT": "0x8ffdD4755aa383cD4CB715ABD8e1375926123729",
         "CHAIN_ID": 168587773,
     },
 }
